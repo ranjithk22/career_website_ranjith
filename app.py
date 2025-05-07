9065e6cb57c33335307c6a1b77033f2bdf382775
ranjithk22 <ranjithkumar.k22@gmail.com>
2025-05-07 17:53:16 +0530

    done

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+jobs = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Hyderabad',
+        'salary': 100000
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Bengalore'
+    },
+    {
+        'id': 3,
+        'title': 'Backend Developer',
+        'location': 'Pune',
+        'salary': 150000
+    },
+]
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html', jobs=jobs)
+
+
+@app.route('/api/jobs')
+def get_jobs():
+    return jsonify(jobs)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
